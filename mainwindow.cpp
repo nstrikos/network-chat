ac7d591dfb950df4a28598672527d72dd0858aa4
nstrikos <nstrikos@yahoo.gr>
2023-04-06 12:16:14 +0300

    ui refactor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <QDebug>
+#include <QKeyEvent>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,7 +10,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->pushButton->setEnabled(false);
+    ui->pushButton_2->setEnabled(false);
     peers = 0;
+    statusBar()->showMessage("Not connected");
+
+    ui->textEdit->installEventFilter(this);
 }
 
 MainWindow::~MainWindow()
@@ -21,20 +26,81 @@ void MainWindow::newParticipant(QString text)
 {
     peers++;
     qDebug()<< text << ": connected";
-    if (peers > 0)
-        ui->pushButton->setEnabled(true);
+    checkButton();
 }
 
 void MainWindow::participantLeft(QString text)
 {
     peers--;
     qDebug() << text << ": disconnected";
-    if (peers <= 0)
-        ui->pushButton->setEnabled(false);
+    checkButton();
+}
+
+bool MainWindow::eventFilter(QObject *object, QEvent *event)
+{
+    if (object == ui->textEdit && event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        if (keyEvent->key() == Qt::Key_Return) {
+            // Special tab handling
+            qDebug("Enter Key Pressed...");
+            activate();
+            return true;
+        } else {
+            return QMainWindow::eventFilter(object, event);
+        }
+    } else {
+        return QMainWindow::eventFilter(object, event);
+    }
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    emit sendText(ui->lineEdit->text());
+    activate();
+}
+
+
+void MainWindow::on_textEdit_2_textChanged()
+{
+    if (ui->textEdit_2->document()->toPlainText() != "")
+        ui->pushButton_2->setEnabled(true);
+}
+
+void MainWindow::checkButton()
+{
+    if (ui->textEdit->document()->toPlainText() != "" && peers > 0)
+        ui->pushButton->setEnabled(true);
+    else
+        ui->pushButton->setEnabled(false);
+
+    if (peers > 0)
+        statusBar()->showMessage("Connected");
+    else
+        statusBar()->showMessage("Not connected");
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+    checkButton();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit_2->clear();
+    ui->pushButton_2->setEnabled(false);
+}
+
+void MainWindow::activate()
+{
+    QString text = ui->textEdit->document()->toPlainText();
+    emit sendText(text);
+    ui->textEdit->clear();
+
+    QTextCursor cursor = QTextCursor(ui->textEdit_2->document());
+    cursor.setPosition(0);
+    ui->textEdit_2->setTextCursor(cursor);
+    ui->textEdit_2->insertPlainText(text + "\n");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
 
 namespace Ui {
 class MainWindow;
@@ -19,15 +20,29 @@ public slots:
     void newParticipant(QString text);
     void participantLeft(QString text);
 
+protected:
+    bool eventFilter(QObject *object, QEvent *event);
+
 signals:
     void sendText(QString text);
 
 private slots:
     void on_pushButton_clicked();
 
+    void on_textEdit_2_textChanged();
+
+    void on_textEdit_textChanged();
+
+    void on_pushButton_2_clicked();
+
+
 private:
     Ui::MainWindow *ui;
     unsigned int peers;
+
+    void activate();
+
+    void checkButton();
 };
 
 #endif // MAINWINDOW_H
